87fe685d934e855e3f41a39418d1fcc2eb2231ac
Charan Vadrevu <35435125+vadrech@users.noreply.github.com>
2022-11-11 02:14:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-#API token = uclapi-14368d4948fb65d-7998a1883fb66c6-218ced184285808-584da96c6f03c13
-#client_id = 7993613939801141.5837972735640426
-#client_secret = e7926d66972661804ea229c464f6f1831499189ad3e5ac553ea2a6cc6b3e4c22
+#API token = ########################################
+#client_id =  ################################
+#client_secret =  ###############################################################
 
 #callback = redirect to page of our site
 #two paths one for login 
